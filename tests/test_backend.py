284293bb88cf43ce281e24fd8799576ef70f22aa
Mridul Bajpai <mridulbajpai25@gmail.com>
2026-07-04 18:12:03 +0530

    Test case

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,9 +1,11 @@
 import minipypy as mini
 
+x = mini.Tensor([1.0, 2.0, 3.0], requires_grad=True)
+z = (x * x).sum()
+z.backward()
+print(x.grad())  # [2, 4, 6]
+
 x = mini.Tensor([2.0], requires_grad=True)
-
-print(x)
-print(x.grad())
-
-x.zero_grad()
-print(x.grad())
+z = (x * x).sum()
+z.backward()
+print(x.grad())  # [4]
